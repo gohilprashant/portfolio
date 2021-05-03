10db7121ff72ddad619cca40316249ea19d2e899
Prashant Gohil <prashantgohil248@gmail.com>
2021-05-03 13:04:03 +0530

    add spinner in portfolio section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-// filter portfolio with filterizr
 $(document).ready(function () {
+    // filter portfolio with filterizr
     $(".filter-menu li").on("click", function () {
         $(this).addClass("active");
         $(this).siblings(".active").removeClass("active");
     });
+
     var filterizd = $(".filtr-container").filterizr({
-        //options object
+        spinner: {
+            enabled: true,
+            fillColor: "#17b978",
+            styles: {
+                height: "75px",
+                margin: "0 auto",
+                width: "75px",
+                "z-index": 2,
+            },
+        },
     });
 });
